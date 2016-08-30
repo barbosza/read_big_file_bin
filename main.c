a8ae166d06a0b80e19e4968b94b464c7f11b04df
Marcos Barbosa <jogosvaoaqui@gmail.com>
2016-08-30 16:30:47 -0300

    revisto

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Lista.h"
-#define ENTITY 41982
+#include <string.h>
+#define ENTITY 2
 #define LOOP 10
-
+#define MB_SIZE 1048576
 
 typedef struct {
     char nome[24];
@@ -12,23 +13,6 @@ typedef struct {
     int faltas;
 }Diario;
 
-int random_number(int min_num, int max_num);
-
-int random_number(int min_num, int max_num)
-        {
-            int result=0,low_num=0,hi_num=0;
-            if(min_num<max_num)
-            {
-                low_num=min_num;
-                hi_num=max_num+1; // this is done to include max_num in output.
-            }else{
-                low_num=max_num+1;// this is done to include max_num in output.
-                hi_num=min_num;
-            }
-            srand(time(NULL));
-            result = (rand()%(hi_num-low_num))+low_num;
-            return result;
-        }
 
 void mostra_float(void *info)
 {
@@ -53,10 +37,11 @@ int main()
     ,"Tiago","Umberto"
     ,"Victor","Xandi"
     };
+
     int a,b,c;
     c = rand() % 10;
     int x,z = 0;
-    Diario dac[ENTITY],aux;
+    Diario dac[ENTITY],aux[ENTITY];
 
 
     printf("%d",sizeof(dac));
@@ -65,6 +50,7 @@ int main()
     for(x=0;x<=LOOP;++x){
         complete = ((double)x/(double)LOOP)*100;
         printf("%2.2lf%% Concluido",complete);
+
         for(z=0;z<ENTITY;++z){
             int media = 0;
             int temp = rand() % 10;
@@ -95,21 +81,22 @@ int main()
     char aluno;
     int y = 0;
     for(y=0;y<LOOP;y++){
-        fseek(p,(sizeof(dac)*y),SEEK_SET);
+        fread(&aux,sizeof(dac),1,p);
+       //fseek(p,(sizeof(dac)*y),SEEK_SET);
         for(x = 0;x<ENTITY;x++){
-                fread(&aux,sizeof(Diario),1,p);
-                if(aux.notas >= 7.0){
+                //fread(&aux,sizeof(Diario),1,p);
+                if(aux[x].notas >= 7.0){
                     aluno = 'A';
-                } else if(aux.notas < 7.0 && aux.notas > 1.3) {
+                } else if(aux[x].notas < 7.0 && aux[x].notas > 1.3) {
                     aluno = 'E';;
                 } else {
                     aluno = 'R';
                 }
-
-                printf("%2lld%22s%22.2lf%5c\n\n",aux.matricula,aux.nome,aux.notas,aluno);
+                printf("%2lld%22s%22.2lf%5c\n\n",aux[x].matricula,aux[x].nome,aux[x].notas,aluno);
         }
+
     }
-    //printf("%ld KB and %d Entries\n\n", fileSize/1024,numEntries);
+    printf("%ld MB and Entries\n\n", fileSize/MB_SIZE);
     fclose(p);
     return 0;
 }
